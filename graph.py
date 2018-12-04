7e9487a9749b8521fb22b7e0146c88b8a9fed56b
Jonathan Wapman <jdwapman@ucdavis.edu>
2018-12-04 12:24:57 -0800

    Implemented graph file reading

--- graph.py
+++ graph.py
@@ -1,22 +1,36 @@
 import gurobipy as grb
+import sys
 
+""" ========== SETUP ========== """
 
-# Initialize edge matrix to 0
-n = 4 # Define number of vertices
+# Check that the program has been called appropriately
+if len(sys.argv) != 2:
+	print "Usage: gurobi graph.py filename.txt"
+	exit()
 
-e = []
-for row in range(n):
+""" ========== READ GRAPH FROM FILE ========== """
 
-	colList = []
-	for col in range(n):
-		colList.append(0)
+# Open the file
+graphFile = open(sys.argv[1], "r")
 
-	e.append(colList)
+# Read the first line of the file as the number of vertices
+n = int(graphFile.readline())
 
-# Create graph (or read from file)
-e[0][1] = 1
-e[0][2] = 1
-e[0][3] = 1
+# Iterate over the file to read in the graph.
+# Note: first line is ignored since it was already read
+graph = {} # Create empty dict to store graph
+for line in graphFile:
+	nums = line.split()
+	source = int(nums[0])
+	dest = int(nums[1])
+
+	# Add to dictionary
+	if source in graph:
+		graph[source].append(dest)
+	else:
+		graph[source] = [dest]
+
+""" ========== GUROBI ILP ========== """
 
 m = grb.Model()
 
@@ -24,21 +38,21 @@ m = grb.Model()
 x = {}
 
 # Create binary variables
-for i in range(n):
+for i in range(1, n+1):
 	x[i] = m.addVar(vtype=grb.GRB.BINARY, name="x%d" % i)
 
-
+# Apply variables to gurobi model
 m.update()
 
 # Add constraints
-for i in range(n):
-	for j in range(n):
-		if e[i][j] == 1:
-			m.addConstr(x[i]+x[j] <= 1)
+for source, dests in graph.iteritems():
+	for dest in dests:
+		m.addConstr(x[source] + x[dest] <= 1)
 
-# Create objective function
-c = [1] * n
-m.setObjective(grb.quicksum(-c[i]*x[i] for i in range(n)))
+# Create objective function. Note: Start from 1 to match file convention.
+# Ignore entry at 0
+c = [1] * (n+1)
+m.setObjective(grb.quicksum(-c[i]*x[i] for i in range(1, n+1)))
 
 m.optimize()
 
